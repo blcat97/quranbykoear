8d84c6e298cbd2ab649adbc4eb7de3e957b759f6
Administrator <Administrator@koear>
2024-09-15 20:13:27 +0400

    init project.

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quranapp/homescreen.dart';
+
+void main() {
+  runApp(MyAPP());
+}
+
+class MyAPP extends StatelessWidget {
+  const MyAPP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+    );
+  }
+}
